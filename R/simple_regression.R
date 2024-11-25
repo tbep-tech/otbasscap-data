57801e2d7ba7bf988372e2b58206b19192e390e7
fawda123 <mbafs2012@gmail.com>
2024-11-25 11:12:23 -0500

    stochastic back-calc for simple regression

--- R/simple_regression.R
+++ R/simple_regression.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(lubridate)
 library(ggplot2)
+library(tidyr)
+library(patchwork)
 library(tbeptools)
 
 load(here::here('data/loads.RData'))
@@ -18,8 +20,8 @@ chldat <- anlz_avedat(epcdata)$mos |>
   inner_join(lddat, by = 'date') |> 
   mutate(
     tn_load_3mo = tn_load + lag(tn_load) + lag(tn_load, 2)
-  ) |> 
-  filter(date >= as.Date('2000-01-01'))
+  )# |> 
+  # filter(date >= as.Date('2000-01-01'))
   
 
 mod <- lm(chla ~ tn_load_3mo, data = chldat)
@@ -30,7 +32,7 @@ tn_load_3mo_9.3 <- (9.3 - mod$coefficients[1]) / mod$coefficients[2]
 # TN at 8.5 chla
 tn_load_3mo_8.5 <- (8.5 - mod$coefficients[1]) / mod$coefficients[2]
 
-p <- ggplot(chldat, aes(x = tn_load_3mo, y = chla)) +
+p1 <- ggplot(chldat, aes(x = tn_load_3mo, y = chla)) +
   geom_point(size = 0.5) +
   geom_smooth(method = 'lm', se = T, formula = y ~ x, color = 'black') +
   geom_segment(
@@ -61,6 +63,68 @@ p <- ggplot(chldat, aes(x = tn_load_3mo, y = chla)) +
     caption = 'Source: EPCHC, Janicki Env.'
   )
 
-png(here::here('figs/simpl_regression.png'), height = 5, width = 5, units = 'in', res = 300)
+stochastic_backcalc <- function(model, y_value, n_draws = 1000) {
+  
+  # Extract coefficient estimates and standard errors
+  coef_estimates <- summary(model)$coefficients
+  intercept <- coef_estimates["(Intercept)", "Estimate"]
+  intercept_se <- coef_estimates["(Intercept)", "Std. Error"]
+  slope <- coef_estimates["tn_load_3mo", "Estimate"]
+  slope_se <- coef_estimates["tn_load_3mo", "Std. Error"]
+  
+  # Generate random draws for intercept and slope
+  intercept_draws <- rnorm(n_draws, intercept, intercept_se)
+  slope_draws <- rnorm(n_draws, slope, slope_se)
+  
+  # Calculate x-values for each draw
+  x_draws <- (y_value - intercept_draws) / slope_draws
+  
+  #
+  return(x_draws)
+  # return(list(
+  #   mean = mean(x_draws),
+  #   median = median(x_draws),
+  #   lower_ci = quantile(x_draws, 0.025),
+  #   upper_ci = quantile(x_draws, 0.975),
+  #   full_draws = x_draws
+  # ))
+
+}
+
+stoc93 <- 
+stoc85 <- 
+
+toplo <- tibble(
+  stoc9.3 = stochastic_backcalc(mod, 9.3),
+  stoc8.5 = stochastic_backcalc(mod, 8.5)
+  ) |> 
+  pivot_longer(everything(), names_to = 'chla', values_to = 'tn_load_3mo') |> 
+  mutate(
+    chla = gsub('stoc', '', chla) |> factor(), 
+    tn_load = 4 * tn_load_3mo
+  )
+medv <- toplo |> 
+  summarise(
+    tn_load = median(tn_load), 
+    .by = chla
+  )
+
+# create ggplot with points jitter and a horizontal line for the median
+p2 <- ggplot(toplo, aes(x = chla, y = tn_load)) + 
+  geom_jitter(width = 0.2, height = 0, size = 0.5) +
+  geom_boxplot(data = medv, width = 0.4, color = 'red') + 
+  theme_minimal() +
+  theme(
+    panel.grid.minor = element_blank()
+  ) + 
+  labs(
+    x = 'Chlorophyll-a (ug/L)',
+    y = 'Annual TN load (tons)',
+    title = 'Stochastic back-calculation of TN load\nfor 9.3 and 8.5 ug/L chl',
+  ) 
+
+p <- p1 + p2 +  plot_layout(ncol = 2)
+
+png(here::here('figs/simple_regression.png'), height = 5, width = 10, units = 'in', res = 300)
 print(p)
 dev.off()
